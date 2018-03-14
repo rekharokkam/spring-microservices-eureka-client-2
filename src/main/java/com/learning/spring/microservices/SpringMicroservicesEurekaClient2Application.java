819ab35bed9964bc6e0d5031d2d301498f752d69
Rekha Rokkam <rekha.rokkam@yahoo.com>
2018-03-14 12:34:13 -0700

    Project starter Kit

---
+++ src/main/java/com/learning/spring/microservices/SpringMicroservicesEurekaClient2Application.java
@@ -0,0 +1,30 @@
+package com.learning.spring.microservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringMicroservicesEurekaClient2Application {
+	
+	@Autowired
+	private EurekaClient client;
+
+	@RequestMapping ("/serviceinfo")
+	public String serviceinfo () {
+		InstanceInfo instanceInfo = client.getNextServerFromEureka("EurekaClient", false);
+		return instanceInfo.getHomePageUrl();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesEurekaClient2Application.class, args);
+	}
+}
